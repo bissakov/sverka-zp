2771f77cddedfabe67cbf9b7b617549225f89964
Alikhan Bissakov <bissakov@yandex.ru>
2023-06-20 20:43:41 +0600

    OOP -> functional

--- src/excel.py
+++ src/excel.py
@@ -1,77 +1,100 @@
+import logging
 import os
 import re
-import shutil
+from datetime import datetime as dt
+from typing import List, Tuple
+
 import openpyxl
 from openpyxl.worksheet.worksheet import Worksheet
-from datetime import datetime as dt
-from typing import List
-from data_structures import ExcelInfo, Dimension
+
+from config import EXCEL_FOLDER
+from data_structures import Dimension
 
 
-class Excel:
-    def __init__(self, excel: ExcelInfo) -> None:
-        self.excel_path = excel.path
-        self.excel_name = excel.name
-        shutil.copyfile(os.path.join(self.excel_path, self.excel_name), os.path.join(r'C:', 'test.xlsx'))
+def get_all_rows(sheet: Worksheet, dim: Dimension) -> List[Tuple]:
+    return [vals for vals in sheet.iter_rows(1, dim.height, 1, dim.width, values_only=True)]
 
-        self.workbook = openpyxl.load_workbook(filename=os.path.join(r'C:', 'test.xlsx'), data_only=True)
-        self.default_sheet = self.workbook.worksheets[0]
-        raw_dimension = Dimension(width=self.default_sheet.max_column, height=self.default_sheet.max_row)
-        self.rows = self.get_all_rows(self.default_sheet, raw_dimension)
 
-        self.new_dimension = Dimension(width=self.get_width(), height=self.get_height())
-        self.rows = [row[0:self.new_dimension.width] for row in self.rows[:self.new_dimension.height]]
+def get_latest_full_cell(row: tuple) -> int:
+    for i, val in enumerate(reversed(row)):
+        if not val:
+            continue
+        return len(row) - i
 
-        self.new_workbook = openpyxl.Workbook()
-        self.new_sheet = self.new_workbook.worksheets[0]
-        self.corrected_name = ''
-        self.date = self.get_date()
 
-    def get_date(self) -> str:
-        months_dict = {1: 'JAN', 2: 'FEV', 3: 'MAR', 4: 'APR', 5: 'MAI', 6: 'IYN',
-                       7: 'JYL', 8: 'AVG', 9: 'SEN', 10: 'OKT', 11: 'NOJ', 12: 'DEK'}
-        date = dt.strptime(re.search(r'\d\d\.\d\d\.\d{4,}', self.rows[0][0]).group(), '%d.%m.%Y')
-        return f'{months_dict[date.month]}_{date.year}'
+def get_width(rows: List[Tuple]) -> int:
+    last_positions = [get_latest_full_cell(row) for row in rows if get_latest_full_cell(row)]
+    return max(set(last_positions), key=last_positions.count)
 
-    def correct(self) -> None:
-        self.xl_copy()
-        self.remove_empty_cols()
-        self.corrected_name = f'Prov_{self.date}.xlsx'
-        new_path = os.path.join(r'C:\Temp', self.corrected_name)
-        open(new_path, 'w').close()
-        self.new_workbook.save(filename=new_path)
-        # shutil.copyfile(src=r'C:\Users\robot.ad\Desktop\Prov_JYL_2022.xlsx', dst=new_path)
 
-    @staticmethod
-    def get_all_rows(sheet: Worksheet, dim: Dimension) -> List[tuple]:
-        return [vals for vals in sheet.iter_rows(1, dim.height, 1, dim.width, values_only=True)]
+def get_height(sheet: Worksheet, rows: List[Tuple]) -> int:
+    return next((len(rows) - i for i, row in enumerate(reversed(rows)) if 'ВСЕГО' in row), sheet.max_row)
 
-    @staticmethod
-    def get_latest_full_cell(row: tuple) -> int:
-        for i, val in enumerate(reversed(row)):
-            if not val:
-                continue
-            return len(row) - i
 
-    def get_width(self) -> int:
-        last_positions = [self.get_latest_full_cell(row) for row in self.rows if self.get_latest_full_cell(row)]
-        return max(set(last_positions), key=last_positions.count)
+def get_dimensions(sheet: Worksheet, rows: List[Tuple]) -> Dimension:
+    return Dimension(width=get_width(rows=rows), height=get_height(sheet, rows=rows))
 
-    def get_height(self) -> int:
-        return next((len(self.rows) - i for i, row in enumerate(reversed(self.rows)) if 'ВСЕГО' in row), self.default_sheet.max_row)
 
-    def xl_copy(self) -> None:
-        for i in range(1, self.new_dimension.height + 1):
-            for j in range(1, self.new_dimension.width + 1):
-                cell = self.default_sheet.cell(row=i, column=j)
-                _ = self.new_sheet.cell(row=i, column=j, value=cell.value)
+def get_date(rows: List[Tuple]) -> str:
+    months_dict = {1: 'JAN', 2: 'FEV', 3: 'MAR', 4: 'APR', 5: 'MAI', 6: 'IYN',
+                   7: 'JYL', 8: 'AVG', 9: 'SEN', 10: 'OKT', 11: 'NOJ', 12: 'DEK'}
+    date = dt.strptime(re.search(r'\d\d\.\d\d\.\d{4,}', rows[0][0]).group(), '%d.%m.%Y')
+    return f'{months_dict[date.month]}_{date.year}'
 
-    def find_empty_columns(self) -> List[int]:
-        st = next(i + 1 for i, col in enumerate(self.rows[3]) if col)
-        cols = self.rows[3][st::]
-        return [i + 1 for i, col in enumerate(cols, start=st) if not col]
 
-    def remove_empty_cols(self) -> None:
-        empty_cols = self.find_empty_columns()
-        for i, col_idx in enumerate(empty_cols):
-            self.new_sheet.delete_cols(col_idx - i, 1)
+def xl_copy(sheet_from: Worksheet, sheet_to: Worksheet, dimension: Dimension) -> None:
+    for i in range(1, dimension.height + 1):
+        for j in range(1, dimension.width + 1):
+            cell = sheet_from.cell(row=i, column=j)
+            _ = sheet_to.cell(row=i, column=j, value=cell.value)
+
+
+def find_empty_columns(rows: List[Tuple]) -> List[int]:
+    st = next(i + 1 for i, col in enumerate(rows[3]) if col)
+    cols = rows[3][st::]
+    return [i + 1 for i, col in enumerate(cols, start=st) if not col]
+
+
+def remove_empty_cols(sheet: Worksheet, rows: List[Tuple]) -> None:
+    empty_cols = find_empty_columns(rows=rows)
+    for i, col_idx in enumerate(empty_cols):
+        sheet.delete_cols(col_idx - i, 1)
+
+
+def correct(excel_name: str) -> Tuple[str, str]:
+    logging.info('Started correcting Excel file.')
+
+    full_path = os.path.join(EXCEL_FOLDER, excel_name)
+
+    workbook = openpyxl.load_workbook(filename=full_path, data_only=True)
+    logging.info(f'Loaded workbook: {full_path}')
+    default_sheet = workbook.worksheets[0]
+    logging.info(f'Loaded default sheet: {default_sheet.title}')
+    raw_dimension = Dimension(width=default_sheet.max_column, height=default_sheet.max_row)
+    logging.info(f'Raw dimension: {raw_dimension}')
+    rows = get_all_rows(default_sheet, raw_dimension)
+    logging.info(f'Got all rows from default sheet. Rows: {len(rows)}')
+
+    new_dimension = get_dimensions(sheet=default_sheet, rows=rows)
+    logging.info(f'New dimension: {new_dimension}')
+    rows = [row[0:new_dimension.width] for row in rows[:new_dimension.height]]
+    logging.info(f'Got all new rows. New rows: {len(rows)}')
+    new_workbook = openpyxl.Workbook()
+    logging.info('Created new workbook.')
+    new_sheet = new_workbook.worksheets[0]
+    logging.info('Created new sheet.')
+    date = get_date(rows=rows)
+    logging.info(f'Got date: {date}')
+
+    xl_copy(sheet_from=default_sheet, sheet_to=new_sheet, dimension=new_dimension)
+    logging.info('Copied data from default sheet to new sheet.')
+    remove_empty_cols(sheet=new_sheet, rows=rows)
+    logging.info('Removed empty columns from new sheet.')
+    corrected_excel_name = f'Prov_{date}.xlsx'
+    corrected_excel_full_path = os.path.join(EXCEL_FOLDER, corrected_excel_name)
+    logging.info(f'Corrected Excel name: {corrected_excel_name}')
+    new_workbook.save(filename=corrected_excel_full_path)
+    logging.info(f'Saved new workbook. Path: {corrected_excel_full_path}')
+    logging.info('Finished correcting Excel file.')
+
+    return corrected_excel_name, date
